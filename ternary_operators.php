608531738dade84c5921f92b7cadf97139b6e49a
Niltal Amal <tataamal1128@gmail.com>
2025-07-12 22:46:05 +0700

    materi tenary dan null coalesing

--- ternary_operators.php
+++ ternary_operators.php
@@ -1,8 +1,21 @@
 <?php
 
 // ternary operator digunakan untuk menyederhanakan percabangan if-else yang sederhana.
-$gaji = 3000000;
-$tabungan = 500000;
+// $gaji = 3000000;
+// $tabungan = 500000;
 
-$uang_jajan = ($gaji >= $tabungan) ? "Bisa beli makan mewah" : "Tidak bisa beli makan";
-echo $uang_jajan . PHP_EOL;
+// $uang_jajan = ($gaji >= $tabungan) ? "Bisa beli makan mewah" : "Tidak bisa beli makan";
+// echo $uang_jajan . PHP_EOL;
+
+// null coalescing operator
+
+$data = [];
+
+// if (isset($data['nama'])) {
+//     echo $nama = $data['nama'];
+// } else {
+//     echo $nama = "Tidak ada nama";
+// }
+
+$nama = $data['nama'] ?? "Tidak ada nama";
+echo $nama . PHP_EOL;

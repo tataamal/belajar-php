c1081317af4f95570909275f4937fbc2b5b7023e
Niltal Amal <tataamal1128@gmail.com>
2025-07-13 20:36:38 +0700

    Class, Object, Properties, Default value

---
+++ OOP/Data/person.php
@@ -0,0 +1,7 @@
+<?php
+
+class Person {
+    var string $name;
+    var int $age;
+    var string $gender = "Unknown"; //set default value to gender property
+}
---
+++ OOP/object.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'Data/person.php';
+
+$person1 = new Person();
+$person1->name = "John Doe";
+$person1->age = 30;
+$person1->gender = 30;
+// var_dump($person1);
+
+echo "Name: " . $person1->name . "\n";
+echo "Age: " . $person1->age . "\n";
+echo "gender: " . $person1->gender . "\n\n";
+
+$person2 = new Person();
+$person2->name = "Sarah Smith";
+$person2->age = 23;
+$person2->gender;
+// var_dump($person1);
+
+echo "Name: " . $person2->name . "\n";
+echo "Age: " . $person2->age . "\n";
+echo "gender: " . $person2->gender . "\n";
+
+// is_int($person1->gender) ? print "ini adalah integer" : print "ini bukan integer";

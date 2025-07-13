7b779ea5825c413e279ae1b4c712381922cfa988
Niltal Amal <tataamal1128@gmail.com>
2025-07-13 23:09:30 +0700

    nullable, keyword this

--- OOP/Data/person.php
+++ OOP/Data/person.php
@@ -1,7 +1,17 @@
 <?php
 
 class Person {
+
+    const AUTHOR = "Niltal Amal";
     var string $name;
-    var int $age;
+    var ?int $age; // allow age to be null
     var string $gender = "Unknown"; //set default value to gender property
+
+    // add function to person class
+    function greeting($name)
+    {
+        echo "Hello, " . $name . "!";
+    }
+
+
 }
--- OOP/object.php
+++ OOP/object.php
@@ -14,12 +14,14 @@ echo "gender: " . $person1->gender . "\n\n";
 
 $person2 = new Person();
 $person2->name = "Sarah Smith";
-$person2->age = 23;
+$person2->age = 59;
 $person2->gender;
+$person2->greeting($person2->name);
 // var_dump($person1);
 
 echo "Name: " . $person2->name . "\n";
 echo "Age: " . $person2->age . "\n";
 echo "gender: " . $person2->gender . "\n";
+echo $person2->greeting($person2->name) . "\n";
 
 // is_int($person1->gender) ? print "ini adalah integer" : print "ini bukan integer";
